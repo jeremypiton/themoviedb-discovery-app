b6b9e48674f2728cd2a16d325849bdb134330d1e
PITON Jérémy <153202474+jeremypiton@users.noreply.github.com>
2026-09-23 16:35:43 +0000

    feat: ✨ ajouter des paramètres de requête pour l'API TMDB et définir des constantes par défaut

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { tmdbAccessToken } from './config';
+import { DEFAULT_LANGUAGE, DEFAULT_PAGE, DEFAULT_REGION } from './constants';
 
 // Create a new express application instance
 const app = express();
@@ -19,6 +20,15 @@ app.listen(port, () => {
 
 // Define a route handler for fetching popular movies from TMDB API
 app.get('/api/movies/popular', async (_req: express.Request, res: express.Response) => {
+  // Create a URLSearchParams object to build the query string for the TMDB API request
+  const queryParams = new URLSearchParams();
+
+  // Extract query parameters from the request and append them to the query string
+  const { language, page, region } = _req.query;
+
+  queryParams.append('language', (language as string) || DEFAULT_LANGUAGE);
+  queryParams.append('page', (page as string) || DEFAULT_PAGE);
+  queryParams.append('region', (region as string) || DEFAULT_REGION);
   try {
     const response = await fetch('https://api.themoviedb.org/3/movie/popular', {
       headers: {
---
+++ src/back-end/schemas/MoviesTypes.ts
@@ -0,0 +1,7 @@
+export type Movie = {
+  id: number;
+  title: string;
+  release_date: string;
+  poster_path: string | null;
+  vote_average: number;
+};
